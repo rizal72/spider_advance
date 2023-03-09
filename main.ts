2d05627d68d2686709452a24f54f8cbae0e175e3
Riccardo Sallusti <riccardo.sallusti@gmail.com>
2023-03-09 22:03:28 +0100

    Aggiorna main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,21 @@
 input.onButtonPressed(Button.A, function () {
-    SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
-    SuperBit.MotorRun(SuperBit.enMotors.M2, -255)
+    SuperBit.MotorRunDual(
+    SuperBit.enMotors.M1,
+    -255,
+    SuperBit.enMotors.M3,
+    -255
+    )
 })
 input.onButtonPressed(Button.AB, function () {
     SuperBit.MotorStopAll()
 })
 input.onButtonPressed(Button.B, function () {
-    SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
-    SuperBit.MotorRun(SuperBit.enMotors.M2, 255)
+    SuperBit.MotorRunDual(
+    SuperBit.enMotors.M1,
+    255,
+    SuperBit.enMotors.M3,
+    255
+    )
 })
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
